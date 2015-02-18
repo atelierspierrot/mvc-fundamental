af132b8d3f825301de2725ea5089a9260d75d3b0
piwi <me@e-piwi.fr>
2015-02-18 21:01:08 +0100

    custom exception classes

---
+++ src/MVCFundamental/Exception/Exception.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * This file is part of the MVC-Fundamental package.
+ *
+ * Copyright (c) 2013-2015 Pierre Cassat <me@e-piwi.fr> and contributors
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * The source code of this package is available online at
+ * <http://github.com/atelierspierrot/mvc-fundamental>.
+ */
+
+namespace MVCFundamental\Exception;
+
+use \MVCFundamental\AppKernel;
+
+/**
+ * Class Exception
+ */
+class Exception
+    extends \Exception
+{
+
+    /**
+     * @param string $message
+     * @param int $code
+     * @param \Exception $previous
+     */
+    public function __construct($message = '', $code = 0, \Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+        try {
+            AppKernel::log(
+                $this->getMessage(), $code, array(
+                'exception'=>$this
+            ), 'error');
+        } catch (\Exception $e) {}
+    }
+
+}
+
+// Endfile
