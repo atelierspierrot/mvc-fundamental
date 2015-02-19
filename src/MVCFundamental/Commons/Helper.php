47476a04b451c6d85235a8e2a6e578ffcae4a932
piwi <me@e-piwi.fr>
2015-02-19 08:37:32 +0100

    review of the global helper

--- src/MVCFundamental/Commons/Helper.php
+++ src/MVCFundamental/Commons/Helper.php
@@ -26,9 +26,79 @@ use \MVCFundamental\FrontController;
 use \Library\Helper\Code as CodeHelper;
 use \Library\Helper\Text as TextHelper;
 
+/**
+ * This is the global app Helper
+ */
 class Helper
 {
 
+    /**
+     * Get the default environment vars to load in views and calling controller's actions
+     * @return array
+     */
+    public static function getDefaultEnvParameters()
+    {
+        return array(
+            'app'               => FrontController::getInstance(),
+            'template_engine'   => FrontController::get('template_engine'),
+            'request'           => FrontController::get('request'),
+            'response'          => FrontController::get('response'),
+            'data'              => FrontController::get('request')->getArguments(),
+        );
+    }
+
+    /**
+     * Test if a class implements an interface
+     * @param $class_name
+     * @param $interface_name
+     * @return bool
+     */
+    public static function classImplements($class_name, $interface_name)
+    {
+        return CodeHelper::implementsInterface($class_name, $interface_name);
+    }
+
+    /**
+     * Get a safe class property name in camel case
+     * @param string $name
+     * @param string $replace
+     * @param bool $capitalize_first_char
+     * @return string
+     */
+    public static function getPropertyName($name = '', $replace = '_', $capitalize_first_char = true)
+    {
+        return TextHelper::toCamelCase($name, $replace, $capitalize_first_char);
+    }
+
+    /**
+     * Get a safe class method name in camel case
+     * @param $property_name
+     * @return string
+     */
+    public static function getMethodName($property_name)
+    {
+        return CodeHelper::getPropertyMethodName($property_name);
+    }
+
+    /**
+     * Call a callback fetching organized arguments depending on its declaration
+     * @param null $method_name
+     * @param null $arguments
+     * @param null $class_name
+     * @return mixed
+     */
+    public static function fetchArguments($method_name = null, $arguments = null, $class_name = null)
+    {
+        return CodeHelper::fetchArguments($method_name, $arguments, $class_name);
+    }
+
+// --------------------------
+// Development utilities
+// --------------------------
+
+    /**
+     * Hard debug anything
+     */
     public static function debug()
     {
         header('Content-Type: text/plain');
@@ -39,34 +109,22 @@ class Helper
         exit('-- debug --');
     }
 
-    public static function getDefaultEnvParameters()
+    /**
+     * Get a safe exception info
+     * @param \Exception $e
+     * @param bool $plain_text
+     */
+    public static function exceptionHandler(\Exception $e, $plain_text = false)
     {
-        return array(
-            'app'               => FrontController::getInstance(),
-            'template_engine'   => FrontController::get('template_engine'),
-            'request'           => FrontController::get('request'),
-            'response'          => FrontController::get('response'),
-        );
-    }
-
-    public static function classImplements($class_name, $interface_name)
-    {
-        return CodeHelper::implementsInterface($class_name, $interface_name);
-    }
-
-    public static function getPropertyName($name = '', $replace = '_', $capitalize_first_char = true)
-    {
-        return TextHelper::toCamelCase($name, $replace, $capitalize_first_char);
-    }
-
-    public static function getMethodName($property_name)
-    {
-        return CodeHelper::getPropertyMethodName($property_name);
-    }
-
-    public static function fetchArguments($method_name = null, $arguments = null, $class_name = null)
-    {
-        return CodeHelper::fetchArguments($method_name, $arguments, $class_name);
+        $nl     = $plain_text ? PHP_EOL : '<br />';
+        $otag   = $plain_text ? '##'.PHP_EOL : '<pre>';
+        $ctag   = $plain_text ? '##'.PHP_EOL : '</pre>';
+        echo $otag,
+            'ERROR : ', get_class($e), $nl,
+            'Message: ', $e->getMessage(), ' (code ', $e->getCode(), ')', $nl,
+            'File: ', $e->getFile(), ':', $e->getLine(), $nl,
+            'Back trace:', $nl, $e->getTraceAsString(), $nl,
+            $ctag;
     }
 
 }
